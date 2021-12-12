82b1bfaf422332144c5a27ffe3810f2224c9107e
Andreas Knüpfer <knue@posteo.de>
2021-12-12 21:21:51 +0100

    Information about the players or one selected player or assign fly privilege

---
+++ player.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+import math
+import numpy as np 
+import time
+import os
+
+import miney
+import miney_toolbox as mtb
+
+if not miney.is_miney_available():
+    raise miney.MinetestRunError("Please start Minetest with the miney game")
+
+if not "MINETEST_USER" in os.environ:
+    print("Please specific the player name in the 'MINETEST_USER' env variable.")
+    exit(1)
+if not "MINETEST_PASSWORD" in os.environ:
+    print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
+    exit(1)
+
+#mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
+mt = miney.Minetest("localhost", 
+    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
+
+print( "Players:")
+for p in mt.player:
+    print( p )
+
+if len(sys.argv) <= 1:
+    exit(0)
+
+playername= sys.argv[1]
+player= mt.player[playername]
+
+if len(sys.argv) <= 2:
+
+    print( "pos ", mtb.pos_as_int(player), ", dir ", player.look_horizontal, 
+        ", quad: ", mtb.quadrant( player ), ", up/down: ", player.look_vertical )
+
+else: ## second arg given
+    if "fly" == sys.argv[2]:
+        print ( "Make player %sd fly" % playername )
+        player.fly= True
