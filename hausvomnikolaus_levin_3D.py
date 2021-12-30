dfe30f75a2e6c61d0bc91eb222b449ef3aff5b2c
Levin Knüpfer <knue@posteo.de>
2021-12-30 11:59:17 +0100

    Levins first part of the 3D version

---
+++ hausvomnikolaus_levin_3D.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+import sys
+import math
+import numpy as np 
+import os
+
+import miney
+import miney_toolbox as mtb
+
+if not miney.is_miney_available():
+    raise miney.MinetestRunError("Please start Minetest with the miney game")
+
+if not "MINETEST_USER" in os.environ:
+    print("Please specific the player name in the 'MINETEST_USER' env variable.")
+    exit(1)
+if not "MINETEST_PASSWORD" in os.environ:
+    print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
+    exit(1)
+
+#mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
+mt = miney.Minetest("localhost", 
+    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
+
+playername= ""
+
+# playername must be given
+if len(sys.argv) > 1:
+    playername= sys.argv[1]
+else:
+    print( "Playername not given, exit" )
+    print( "Available players are" )
+    for p in mt.player:
+        print( p )
+    exit(1)
+
+player= mt.player[playername]
+
+# haus vom nikolaus
+pos= mtb.pos_as_int( player )
+material= "wool:blue"
+
+a=20
+h=10
+
+material= "wool:blue"
+polygon= []
+# nur eine Dimension ändert sich pro Zug
+polygon.append( [0,0,0] )
+polygon.append( [0,a,0] )
+polygon.append( [a,a,0] )
+polygon.append( [a,0,0] )
+polygon.append( [a,0,a] )
+polygon.append( [0,0,a] )
+polygon.append( [0,a,a] )
+polygon.append( [a,a,a] )
+
+for i in range(len(polygon)-1):
+    print(i,polygon[i],polygon[i+1])
+    mtb.line( mt, pos, np.array(polygon[i]), np.array(polygon[i+1]), material )
+
+material= "wool:yellow"
+polygon= []
+# zwei Dimension ändern sich pro Zug
+polygon.append( [a,a,a] )
+polygon.append( [a,a,0] )
+polygon.append( [h,a+h,h] )
+polygon.append( [0,a,a] )
+polygon.append( [a,0,a] )
+polygon.append( [a,a,0] )
+polygon.append( [0,0,0] )
+
+for i in range(len(polygon)-1):
+    print(i,polygon[i],polygon[i+1])
+    mtb.line( mt, pos, np.array(polygon[i]), np.array(polygon[i+1]), material )
+
+material= "wool:orange"
+polygon= []
+# zwei Dimension ändern sich pro Zug
+polygon.append( [0,0,0] )
+polygon.append( [0,0,a] )
+polygon.append( [a,0,0] )
+
+for i in range(len(polygon)-1):
+    print(i,polygon[i],polygon[i+1])
+    mtb.line( mt, pos, np.array(polygon[i]), np.array(polygon[i+1]), material )
+
+material= "wool:green"
+polygon= []
+# zwei Dimension ändern sich pro Zug
+polygon.append( [a,0,0] )
+polygon.append( [0,0,0] )
+polygon.append( [0,a,a] )
+
+for i in range(len(polygon)-1):
+    print(i,polygon[i],polygon[i+1])
+    mtb.line( mt, pos, np.array(polygon[i]), np.array(polygon[i+1]), material )
+
