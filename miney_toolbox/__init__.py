72da07f7e356060842456f22e973d65c9491072f
Konrad Knüpfer <knue@posteo.de>
2024-01-20 16:54:22 +0100

    Add empty circle function

--- miney_toolbox/__init__.py
+++ miney_toolbox/__init__.py
@@ -1,5 +1,5 @@
 from .box import box
-from .circle import circle
+from .circle import circle, circle_empty
 from .conv import ntom
 from .line import line
 from .query import pos, pos_as_int, quadrant
--- miney_toolbox/circle.py
+++ miney_toolbox/circle.py
@@ -28,3 +28,29 @@ def circle( mt, pos, r, h, material ):
                     positions.append( conv.ntom( midpoint - x*vx - z*vz ) )
 
     mt.node.set( nodes= positions, name= material )
+
+
+""" create a horizontal circle with radius r filled in with the given material,
+the base is placed one step below the given position """
+def circle_empty( mt, pos, r, h, material ):
+
+    positions = []
+
+    vx= np.array([1,0,0])
+    vy= np.array([0,1,0])
+    vz= np.array([0,0,1])
+
+    midpoint= pos + h*vy
+
+    for x in range(0,int(r)+2):
+        for z in range(0,int(r)+2):
+
+                d= x*vx + z*vz
+                a= np.linalg.norm( d )
+                if r - 0.5 - 0.5 <=a and a < r + 0.5 :
+                    positions.append( conv.ntom( midpoint + x*vx + z*vz ) )
+                    positions.append( conv.ntom( midpoint + x*vx - z*vz ) )
+                    positions.append( conv.ntom( midpoint - x*vx + z*vz ) )
+                    positions.append( conv.ntom( midpoint - x*vx - z*vz ) )
+
+    mt.node.set( nodes= positions, name= material )
