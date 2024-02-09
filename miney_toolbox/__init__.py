58d75b10b7684e31b085ec2859d893266eefa049
Andreas Knüpfer <knue@posteo.de>
2024-02-09 17:55:09 +0100

    Game of life in Minetest

--- miney_toolbox/__init__.py
+++ miney_toolbox/__init__.py
@@ -3,5 +3,5 @@ from .circle import circle, circle_empty
 from .conv import ntom
 from .line import line
 from .query import pos, pos_as_int, quadrant
-from .rect import rect
+from .rect import rect, rect_field, rect_field_backbuffer
 from .sphere import sphere
