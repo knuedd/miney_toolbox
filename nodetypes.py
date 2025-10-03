e463ec0299c620462c46f33964c6787ee126a34a
knue <knue@posteo.de>
2025-10-03 12:20:29 +0200

    Port to Luanti

--- nodetypes.py
+++ nodetypes.py
@@ -9,19 +9,20 @@ import os
 import miney
 import miney_toolbox as mtb
 
-if not miney.is_miney_available():
-    raise miney.MinetestRunError("Please start Minetest with the miney game")
-
 if not "MINETEST_USER" in os.environ:
     print("Please specific the player name in the 'MINETEST_USER' env variable.")
     exit(1)
+
 if not "MINETEST_PASSWORD" in os.environ:
     print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
     exit(1)
 
-#mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
-mt = miney.Minetest("localhost", 
-    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
+try:
+    with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
 
-for node_type in mt.node.type:
-    print(node_type)
+        for node_type in mt.nodes.names:
+            print(node_type)
+        print(f"We have {len(mt.nodes.names)} node types")
+
+except Exception as e:
+        print(f"An unexpected error occurred: {e}")
