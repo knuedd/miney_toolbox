105ddfef4738460f3d20788ab3fb1ddd7e2a49c7
knue <knue@posteo.de>
2025-10-03 17:03:21 +0200

    Port to Luanti class

--- miney_toolbox/conv.py
+++ miney_toolbox/conv.py
@@ -1,10 +1,13 @@
 import math
 import numpy as np 
-import miney
-from miney import Minetest
+import miney 
 import time
 
 """ transform numpy vector of len 3 to minetest position """
 def ntom( f ):
 
     return { "x": float(f[0]), "y": float(f[1]), "z": float(f[2]) }
+
+def ntonode( f, material="mcl_wool:red" ):
+
+    return miney.node.Node( float(f[0]), float(f[1]), float(f[2]), name=material )
