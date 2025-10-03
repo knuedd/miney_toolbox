11b39cf3af9a69ade1b783e2cb60b38d7f9b97da
knue <knue@posteo.de>
2025-10-03 11:56:16 +0200

    Port to Luanti

--- timeofday.py
+++ timeofday.py
@@ -7,9 +7,6 @@ import os
 
 import miney
 
-if not miney.is_miney_available():
-    raise miney.MinetestRunError("Please start Minetest with the miney game")
-
 if not "MINETEST_USER" in os.environ:
     print("Please specific the player name in the 'MINETEST_USER' env variable.")
     exit(1)
@@ -18,11 +15,16 @@ if not "MINETEST_PASSWORD" in os.environ:
     exit(1)
 
 #mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
-mt = miney.Minetest("localhost", 
-    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
 
-if len(sys.argv) > 1:
-    time= int( sys.argv[1] )
-    mt.time_of_day= time/24.0
-else:
-    print( "Specify time of day as hour between 0 and 23")
+try:
+    with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
+
+        if len(sys.argv) > 1:
+            time= int( sys.argv[1] )
+            mt.time_of_day= time/24.0
+        else:
+            print( "Specify time of day as hour between 0 and 23")
+
+except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
