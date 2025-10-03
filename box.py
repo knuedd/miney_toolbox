4d7727f9f6e17c2b108e44264c167a6b8a54ebf1
knue <knue@posteo.de>
2025-10-03 16:45:32 +0200

    Manual box pattern, direct without toolbox functionality

--- box.py
+++ box.py
@@ -8,9 +8,6 @@ import os
 import miney
 import miney_toolbox as mtb
 
-if not miney.is_miney_available():
-    raise miney.MinetestRunError("Please start Minetest with the miney game")
-
 if not "MINETEST_USER" in os.environ:
     print("Please specific the player name in the 'MINETEST_USER' env variable.")
     exit(1)
@@ -18,43 +15,56 @@ if not "MINETEST_PASSWORD" in os.environ:
     print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
     exit(1)
 
-#mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
-mt = miney.Minetest("localhost", 
-    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
+try:
+    with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
 
-playername= ""
-material= "wool:red"
+        playername= ""
+        material= "mcl_wool:red"
 
-# playername must be given
-if len(sys.argv) > 1:
-    playername= sys.argv[1]
-else:
-    print( "Playername not given, exit" )
-    print( f"call as {sys.argv[0]} <playername> <width> <depth> <height>" )
-    print( "Available players are" )
-    for p in mt.player:
-        print( p )
-    exit(1)
+        # playername must be given
+        if len(sys.argv) > 1:
+            playername= sys.argv[1]
+        else:
+            print( "Playername not given, exit" )
+            print( f"call as {sys.argv[0]} <playername> <width> <depth> <height>" )
+            print( "Available players are" )
+            for p in mt.players:
+                print( p )
+            exit(1)
 
-player= mt.player[playername]
+        player= mt.players[playername]
 
-# if start and stop are not given print the players position and direction of view
-if len(sys.argv) > 3:
+        # if start and stop are not given print the players position and direction of view
+        if len(sys.argv) > 3:
 
-    w= int( sys.argv[2] )
-    d= int( sys.argv[3] )
-    h= int( sys.argv[4] )
+            w= int( sys.argv[2] )
+            d= int( sys.argv[3] )
+            h= int( sys.argv[4] )
 
-    if len(sys.argv) > 5:
-        material= sys.argv[5]
+            if len(sys.argv) > 5:
+                material= sys.argv[5]
 
-    mtb.box( mt, mtb.pos_as_int( player ), w, d, h, material )
+            nodelist= []
 
-else:
-    print( f"call as {sys.argv[0]} {sys.argv[1]} <width> <depth> <height>" )
-    print( "" )
-    print( f"Info: Player {playername}" )
-    print( "    Position  ", mtb.pos_as_int(player) )
-    print( "    Direction ", player.look_horizontal, player.look_vertical )
-    print( "    Quadrant  ", mtb.quadrant( player ) )
+            playerpos= mtb.pos_as_int(player)
 
+            for x in range(w):
+                for z in range(d):
+                    for y in range(h):
+
+                        pos= playerpos + [x,y,z]
+                        print(pos)
+                        nodelist.append( mtb.ntonode( pos, material ) ) 
+
+            mt.nodes.set( nodelist)
+
+        else:
+            print( f"call as {sys.argv[0]} {sys.argv[1]} <width> <depth> <height>" )
+            print( "" )
+            print( f"Info: Player {playername}" )
+            print( "    Position  ", mtb.pos_as_int(player) )
+            print( "    Direction ", player.look_horizontal, player.look_vertical )
+            print( "    Quadrant  ", mtb.quadrant( player ) )
+
+except Exception as e:
+        print(f"An unexpected error occurred: {e}")
--- miney_toolbox/__init__.py
+++ miney_toolbox/__init__.py
@@ -4,7 +4,7 @@ Miney Toolbox provides higher level functionalities on top of Miney
 
 from .box import box
 from .circle import circle, circle_empty
-from .conv import ntom
+from .conv import ntom, ntonode
 from .line import line, set
 from .query import pos, pos_as_int, quadrant
 from .rect import rect, rect_field, rect_field_backbuffer
