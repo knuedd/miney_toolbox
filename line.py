79a3922b16b969398e93e496034cb4e7e3dfed7d
knue <knue@posteo.de>
2025-10-28 17:28:11 +0100

    Make line use the Luanti class

--- line.py
+++ line.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import sys
 import math
@@ -8,9 +8,6 @@ import os
 import miney
 import miney_toolbox as mtb
 
-if not miney.is_miney_available():
-    raise miney.MinetestRunError("Please start Minetest with the miney game")
-
 if not "MINETEST_USER" in os.environ:
     print("Please specific the player name in the 'MINETEST_USER' env variable.")
     exit(1)
@@ -18,52 +15,50 @@ if not "MINETEST_PASSWORD" in os.environ:
     print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
     exit(1)
 
-#mt = mt.Minetest( "localhost", "playername", "password", port= 29999 )
-mt = miney.Minetest("localhost", 
-    os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] )
+with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
 
-if not miney.is_miney_available():
-    raise miney.MinetestRunError("Please start Minetest with the miney game")
+    playername= ""
+    material= "mcl_wool:red"
 
-playername= ""
-material= "wool:red"
+    playername= ""
+    material= "wool:red"
 
-# playername must be given
-if len(sys.argv) > 1:
-    playername= sys.argv[1]
-else:
-    print( "Playername not given, exit" )
-    print( "Available players are" )
-    for p in mt.player:
-        print( p )
-    exit(1)
+    # playername must be given
+    if len(sys.argv) > 1:
+        playername= sys.argv[1]
+    else:
+        print( "Playername not given, exit" )
+        print( "Available players are" )
+        for p in mt.player:
+            print( p )
+        exit(1)
 
-player= mt.player[playername]
+    player= mt.players[playername]
 
-# if start and stop are not given print the players position and direction of view
-if len(sys.argv) > 7:
+    # if start and stop are not given print the players position and direction of view
+    if len(sys.argv) > 7:
 
-    x0= int( sys.argv[2] )
-    y0= int( sys.argv[3] )
-    z0= int( sys.argv[4] )
-    x1= int( sys.argv[5] )
-    y1= int( sys.argv[6] )
-    z1= int( sys.argv[7] )
+        x0= int( sys.argv[2] )
+        y0= int( sys.argv[3] )
+        z0= int( sys.argv[4] )
+        x1= int( sys.argv[5] )
+        y1= int( sys.argv[6] )
+        z1= int( sys.argv[7] )
 
-    if len(sys.argv) > 8:
-        material= sys.argv[8]
+        if len(sys.argv) > 8:
+            material= sys.argv[8]
 
-    start= np.array([x0,y0,z0])
-    end=   np.array([x1,y1,z1])
+        start= np.array([x0,y0,z0])
+        end=   np.array([x1,y1,z1])
 
-    print( "Line from ", start, " to ", end, 
-        " relative to ", mtb.pos_as_int( player ) )
+        print( "Line from ", start, " to ", end, 
+            " relative to ", mtb.pos_as_int( player ) )
 
-    mtb.line( mt, mtb.pos_as_int( player ), start, end, material )
+        mtb.line( mt, mtb.pos_as_int( player ), start, end, material )
 
-else:
-    print( "Player %s" % playername )
-    print( "    Position  ", mtb.pos_as_int(player) )
-    print( "    Direction ", player.look_horizontal, player.look_vertical )
-    print( "    Quadrant  ", mtb.quadrant( player ) )
+    else:
+        print( "Player %s" % playername )
+        print( "    Position  ", mtb.pos_as_int(player) )
+        print( "    Direction ", player.look_horizontal, player.look_vertical )
+        print( "    Quadrant  ", mtb.quadrant( player ) )
 
--- miney_toolbox/__init__.py
+++ miney_toolbox/__init__.py
@@ -2,13 +2,15 @@
 Miney Toolbox provides higher level functionalities on top of Miney
 """
 
-from .box import box
-from .circle import circle, circle_empty
-from .conv import ntom, ntonode
-from .line import line, set
 from .query import pos, pos_as_int, quadrant
-from .rect import rect, rect_field, rect_field_backbuffer
-from .sphere import sphere
-from .copyandpaste import read, copy, paste
+from .conv import ntom, ntonode
+from .box import box
+from .line import line, set
+
+# from .circle import circle, circle_empty
+# from .conv import ntom, ntonode
+# from .rect import rect, rect_field, rect_field_backbuffer
+# from .sphere import sphere
+# from .copyandpaste import read, copy, paste
 
 __version__ = "0.1.0"
