099989eb02703240cba82b8ca7984edd395d5f5b
knue <knue@posteo.de>
2025-10-28 17:02:41 +0100

    Box script directly via Luanti object

--- box.py
+++ box.py
@@ -15,56 +15,53 @@ if not "MINETEST_PASSWORD" in os.environ:
     print("Please specific the player name in the 'MINETEST_PASSWORD' env variable.")
     exit(1)
 
-try:
-    with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
 
-        playername= ""
-        material= "mcl_wool:red"
+with miney.Luanti("localhost", os.environ['MINETEST_USER'], os.environ['MINETEST_PASSWORD'] ) as mt:
 
-        # playername must be given
-        if len(sys.argv) > 1:
-            playername= sys.argv[1]
-        else:
-            print( "Playername not given, exit" )
-            print( f"call as {sys.argv[0]} <playername> <width> <depth> <height>" )
-            print( "Available players are" )
-            for p in mt.players:
-                print( p )
-            exit(1)
+    playername= ""
+    material= "mcl_wool:red"
 
-        player= mt.players[playername]
+    # playername must be given
+    if len(sys.argv) > 1:
+        playername= sys.argv[1]
+    else:
+        print( "Playername not given, exit" )
+        print( f"call as {sys.argv[0]} <playername> <width> <depth> <height>" )
+        print( "Available players are" )
+        for p in mt.players:
+            print( p )
+        exit(1)
 
-        # if start and stop are not given print the players position and direction of view
-        if len(sys.argv) > 3:
+    player= mt.players[playername]
 
-            w= int( sys.argv[2] )
-            d= int( sys.argv[3] )
-            h= int( sys.argv[4] )
+    # if start and stop are not given print the players position and direction of view
+    if len(sys.argv) > 3:
 
-            if len(sys.argv) > 5:
-                material= sys.argv[5]
+        w= int( sys.argv[2] )
+        d= int( sys.argv[3] )
+        h= int( sys.argv[4] )
 
-            nodelist= []
+        if len(sys.argv) > 5:
+            material= sys.argv[5]
 
-            playerpos= mtb.pos_as_int(player)
+        nodelist= []
 
-            for x in range(w):
-                for z in range(d):
-                    for y in range(h):
+        playerpos= mtb.pos_as_int(player)
 
-                        pos= playerpos + [x,y,z]
-                        print(pos)
-                        nodelist.append( mtb.ntonode( pos, material ) ) 
+        for x in range(w):
+            for z in range(d):
+                for y in range(h):
 
-            mt.nodes.set( nodelist)
+                    pos= playerpos + [x,-y,z]
+                    print(pos)
+                    nodelist.append( mtb.ntonode( pos, material ) ) 
 
-        else:
-            print( f"call as {sys.argv[0]} {sys.argv[1]} <width> <depth> <height>" )
-            print( "" )
-            print( f"Info: Player {playername}" )
-            print( "    Position  ", mtb.pos_as_int(player) )
-            print( "    Direction ", player.look_horizontal, player.look_vertical )
-            print( "    Quadrant  ", mtb.quadrant( player ) )
+        mt.nodes.set( nodelist )
 
-except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    else:
+        print( f"call as {sys.argv[0]} {sys.argv[1]} <width> <depth> <height>" )
+        print( "" )
+        print( f"Info: Player {playername}" )
+        print( "    Position  ", mtb.pos_as_int(player) )
+        print( "    Direction ", player.look_horizontal, player.look_vertical )
+        print( "    Quadrant  ", mtb.quadrant( player ) )
